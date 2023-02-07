cd4a0a80f25c3b08793e24eb4352bac1d44a1473
Qagura <quddusnuriman@gmail.com>
2023-02-07 22:10:08 +0800

    tictactoe

--- public/tictactoe/ticstyle.js
+++ public/tictactoe/ticstyle.js
@@ -38,6 +38,8 @@ function startgame() {
         computermove();
         roulette = 0;
       }
+
+      whoWin();
     } else {
       for (let i = 0; i < grids.length; i++) {
         grids[i].innerHTML = null;
@@ -49,47 +51,81 @@ function startgame() {
     }
   };
 
-  function playermove() {
-    turn.innerHTML = "Your turn!";
-    for (let i = 0; i < grids.length; i++) {
-      grids[i].onclick = function () {
-        if (grids[i].innerHTML != "O" && grids[i].innerHTML != "X") {
-          grids[i].innerHTML = "X";
-          filled += 1;
-          playerturn = false;
-          computerturn = true;
-          swapturn(playerturn, computerturn);
-        }
-      };
-    }
-  }
+}
 
-  async function computermove() {
-    turn.innerHTML = "Computer's turn!";
-    while (true) {
-      let random = Math.floor(Math.random() * grids.length);
-      console.log(random);
-      if (grids[random].innerHTML != "O" && grids[random].innerHTML != "X") {
-        await new Promise((r) => setTimeout(r, 1100));
-        grids[random].innerHTML = "O";
+function playermove() {
+  turn.innerHTML = "Your turn!";
+  for (let i = 0; i < grids.length; i++) {
+    grids[i].onclick = function () {
+      if (grids[i].innerHTML != "O" && grids[i].innerHTML != "X") {
+        grids[i].innerHTML = "X";
         filled += 1;
-        computerturn = false;
-        playerturn = true;
+        playerturn = false;
+        computerturn = true;
         swapturn(playerturn, computerturn);
-        break;
       }
-      if (filled == 9) {
-        turn.innerHTML = "Game ended";
-        break;
-      }
-    }
+    };
   }
+}
 
-  function swapturn(playerturn, computerturn) {
-    if (playerturn && !computerturn) {
-      playermove();
-    } else {
-      computermove();
+async function computermove() {
+  turn.innerHTML = "Computer's turn!";
+  while (true) {
+    let random = Math.floor(Math.random() * grids.length);
+    console.log(random);
+    if (grids[random].innerHTML != "O" && grids[random].innerHTML != "X") {
+      await new Promise((r) => setTimeout(r, 1100));
+      grids[random].innerHTML = "O";
+      filled += 1;
+      computerturn = false;
+      playerturn = true;
+      swapturn(playerturn, computerturn);
+      break;
+    }
+    if (filled == 9) {
+      turn.innerHTML = "Game ended";
+      break;
     }
   }
 }
+
+function swapturn(playerturn, computerturn) {
+  if (playerturn && !computerturn) {
+    playermove();
+  } else {
+    computermove();
+  }
+}
+
+function whoWin() {
+  if(
+    (grids[0]==grids[1]==grids[2] == "X") ||
+    (grids[0]==grids[3]==grids[6] == "X") ||
+    (grids[0]==grids[4]==grids[8] == "X") ||
+    (grids[1]==grids[4]==grids[7] == "X") ||
+    (grids[2]==grids[5]==grids[8] == "X") ||
+    (grids[2]==grids[4]==grids[6] == "X") ||
+    (grids[3]==grids[4]==grids[5] == "X") ||
+    (grids[6]==grids[7]==grids[8] == "X")
+  ){
+    turn.innerHTML = "you win!!";
+  }else if(
+    (grids[0]==grids[1]==grids[2] == "O") ||
+    (grids[0]==grids[3]==grids[6] == "O") ||
+    (grids[0]==grids[4]==grids[8] == "O") ||
+    (grids[1]==grids[4]==grids[7] == "O") ||
+    (grids[2]==grids[5]==grids[8] == "O") ||
+    (grids[2]==grids[4]==grids[6] == "O") ||
+    (grids[3]==grids[4]==grids[5] == "O") ||
+    (grids[6]==grids[7]==grids[8] == "O")
+  ){
+    turn.innerHTML = "computer win!!";
+  }else{
+    turn.innerHTML = "draw!!";
+  }
+}
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+
