8e9c68f80f6597eaa131c8295ffb07f2aa089c5a
Qagura <quddusnuriman@gmail.com>
2023-02-14 19:07:08 +0800

    tictactoe

--- public/tictactoe/ticstyle.js
+++ public/tictactoe/ticstyle.js
@@ -1,13 +1,13 @@
 startgame();
 
-function startgame(){
+function startgame() {
   let turn = document.querySelector("#turntext");
   let button = document.querySelector("#start");
-  
+
   let clicked = false;
   let gameEnd = false;
   let hasMoved = false;
-  
+
   let grid1 = document.querySelector("#grid1");
   let grid2 = document.querySelector("#grid2");
   let grid3 = document.querySelector("#grid3");
@@ -18,27 +18,26 @@ function startgame(){
   let grid8 = document.querySelector("#grid8");
   let grid9 = document.querySelector("#grid9");
   let grids = [grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9];
-  
+
   button.onclick = function () {
     if (!clicked) {
       clicked = true;
       let playerturn = false;
       let computerturn = false;
-  
+
       button.innerHTML = "restart";
       let roulette = Math.round(Math.random());
-  
-      if(roulette === 0){
+
+      if (roulette === 0) {
         playerturn = true;
         playermove();
         roulette = 1;
-      }else{
+      } else {
         computerturn = true;
         computermove();
         roulette = 0;
       }
-     
-    }else{
+    } else {
       for (let i = 0; i < grids.length; i++) {
         grids[i].innerHTML = null;
       }
@@ -47,12 +46,12 @@ function startgame(){
       startgame();
     }
   };
-  
-  function playermove(){
+
+  function playermove() {
     turn.innerHTML = "Your turn!";
-    for(let i = 0; i<grids.length; i++){
-      grids[i].onclick = function(){
-        if(grids[i].innerHTML != "O" && grids[i].innerHTML != "X"){
+    for (let i = 0; i < grids.length; i++) {
+      grids[i].onclick = function () {
+        if (grids[i].innerHTML != "O" && grids[i].innerHTML != "X") {
           grids[i].innerHTML = "X";
           playerturn = false;
           computerturn = true;
@@ -61,14 +60,14 @@ function startgame(){
       };
     }
   }
-  
-  async function computermove(){
+
+  async function computermove() {
     turn.innerHTML = "Computer's turn!";
-    while(true){
+    while (true) {
       let random = Math.floor(Math.random() * grids.length);
       console.log(random);
-      if (grids[random].innerHTML != "O" && grids[random].innerHTML != "X"){
-        await new Promise(r => setTimeout(r, 1500));
+      if (grids[random].innerHTML != "O" && grids[random].innerHTML != "X") {
+        await new Promise((r) => setTimeout(r, 1500));
         grids[random].innerHTML = "O";
         computerturn = false;
         playerturn = true;
@@ -77,13 +76,12 @@ function startgame(){
       }
     }
   }
-  
-  function swapturn(playerturn, computerturn){
-    if(playerturn && !computerturn){
+
+  function swapturn(playerturn, computerturn) {
+    if (playerturn && !computerturn) {
       playermove();
-    }else{
+    } else {
       computermove();
     }
   }
 }
-
