4bb2cf8103070a47d8d034d8e4394c8226f49c57
Qagura <quddusnuriman@gmail.com>
2023-02-23 01:44:04 +0800

    recovered

---
+++ public/index.js
@@ -0,0 +1,7 @@
+document.querySelector("#edbutton").onclick = function () {
+    location.href = "#page2";
+}
+
+document.querySelector("#artbutton").onclick = function () {
+    location.href = "#page3";
+}
