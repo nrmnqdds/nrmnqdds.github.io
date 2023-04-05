d9ac21748343e1d2d42c43d5f06c5707c88fe1db
qud <quddusnuriman@gmail.com>
2023-04-06 00:22:51 +0800

    modified js for tictactoe

--- tictactoe/ticstyle.js
+++ tictactoe/ticstyle.js
@@ -1,6 +1,37 @@
 
+let grid1 = document.querySelector("#grid1");
+let grid2 = document.querySelector("#grid2");
+let grid3 = document.querySelector("#grid3");
+let grid4 = document.querySelector("#grid4");
+let grid5 = document.querySelector("#grid5");
+let grid6 = document.querySelector("#grid6");
+let grid7 = document.querySelector("#grid7");
+let grid8 = document.querySelector("#grid8");
+let grid9 = document.querySelector("#grid9");
+
+let grids = [grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9];
+
+let startbtn = document.querySelector("#start");
+let turntext = document.querySelector("#turntext");
+
+let started = false;
+
+startbtn.addEventListener("click", function(){
+  console.log("clicked");
+})
+
 startgame();
 
 function startgame(){
-  document.querySelector("#start").textContent = "Start";
+  // startbtn.textContent = "Start";
+  turntext.textContent = "press play";
+
+
+  for (let i = 0; i < grids.length; i++) {
+    grids[i].textContent = "";
+  }
+  
+
+
 }
+
