ad81526b0c50495eb4d11c38530b37af18a5f30e
Qagura <quddusnuriman@gmail.com>
2023-05-11 11:11:06 +0800

    added few components

---
+++ public/components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { SocialIcon } from "react-social-icons";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <header>
+        <div className="flex flex-row items-center">
+          <SocialIcon
+            url="https://twitter.com/nrmnqdds"
+            fgColor="#515151"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://github.com/qryskalyst20"
+            fgColor="#515151"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://instagram.com/nrmnqdds"
+            fgColor="#515151"
+            bgColor="transparent"
+          />
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ public/components/IntroductionCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class IntroductionCard extends Component {
+  state = {};
+  render() {
+    return <div className="bg-white">introcard</div>;
+  }
+}
+
+export default IntroductionCard;
