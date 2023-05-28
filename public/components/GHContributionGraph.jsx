f89d3e8ac68b3356aab54816505e52bdbb08024e
Qagura <quddusnuriman@gmail.com>
2023-05-28 23:41:19 +0800

    added motion.div

---
+++ public/components/GHContributionGraph.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { motion } from "framer-motion";
+
+// Custom component for image element
+const Image = ({ node, ...props }) => {
+  return <img style={{ borderRadius: "10px" }} {...props} />;
+};
+
+export default function GHContributonGraph() {
+  return (
+    <motion.div className="w-[30%] rounded-xl" whileHover={{ scale: 1.05 }}>
+      <ReactMarkdown
+        components={{
+          // Use the custom component for image element
+          img: Image,
+        }}
+      >
+        [![Ashutosh's github activity
+        graph](https://github-readme-activity-graph.vercel.app/graph?username=qryskalyst20&bg_color=ffcfe9&color=9e4c98&line=9e4c98&point=403d3d&area=true&hide_border=true&padding=20&hide_title=true&hide_legend=true&hide_labels=true)](https://github.com/qryskalyst20)
+      </ReactMarkdown>
+    </motion.div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
-"use client";
-
 import React from "react";
 import localFont from "next/font/local";
+import GHContributonGraph from "../../public/components/GHContributionGraph";
 
 const SFBold = localFont({
   src: "../../public/fonts/SF-Pro-Text-Bold.otf",
@@ -13,10 +12,11 @@ export default function Home() {
     <div
       className={`${SFBold.className} flex flex-col min-h-screen w-screen bg-[#121212] p-10`}
     >
-      <div className="text-left">
-        <p className="text-4xl">Hello! I am Quddus!</p>
+      <div className="text-left m-10 ml-0">
+        <p className="text-6xl">Hello! I am Quddus!</p>
         <p>20 years old IIUM Student</p>
       </div>
+      <GHContributonGraph />
     </div>
   );
 }
