a669afa2a942c9cc0262e8a181832e0cd0c7be8f
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 12:02:13 +0800

    :sparkles: Creates dark mode toggle button

---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
+import React from "react";
+
+type ThemeSwitcherProps = {
+  className?: string;
+};
+
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      className={`w-fit p-2 rounded-md hover:scale-110 active:scale-100 duration-200 text-zinc-900 dark:text-slate-200 text-2xl font-bold ${className}`}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "light" ? <Moon /> : <Sun />}
+    </button>
+  );
+};
