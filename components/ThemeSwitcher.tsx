553937c6622c026f27b4dbfa650d16b6f4baf250
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 12:38:20 +0800

    :lipstick: Adds border to ThemeSwitcher

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -8,7 +8,7 @@ type ThemeSwitcherProps = {
   className?: string;
 };
 
-export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const [mounted, setMounted] = useState<boolean>(false);
   const { theme, setTheme } = useTheme();
   useEffect(() => {
@@ -20,10 +20,12 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
   return (
     <button
-      className={`w-fit p-2 rounded-md hover:scale-110 active:scale-100 duration-200 text-zinc-900 dark:text-slate-200 text-2xl font-bold ${className}`}
+      className={`w-fit p-2 rounded-md hover:scale-110 active:scale-100 duration-200 dark:bg-zinc-900 border border-solid border-gray-600 bg-slate-200 text-zinc-900 dark:text-slate-200 text-2xl font-bold ${className}`}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {theme === "light" ? <Moon /> : <Sun />}
     </button>
   );
 };
+
+export default ThemeSwitcher;
