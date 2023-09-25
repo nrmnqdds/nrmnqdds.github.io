1181dc618c8eccc826ef26ec689e4f4eca3c71b4
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 12:37:21 +0800

    :truck: Moves Navbar to HOC

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/providers/themeProvider";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "QUDDUS | Software Developer",
+  title: "Quddus | Software Developer",
   description: "Turn your ideas into reality with me.",
 };
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
