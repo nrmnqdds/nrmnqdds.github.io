8993664beb8976974e5abdf492a52e099eb28844
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 12:37:44 +0800

    :truck: Moves ThemeSwitcher into navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { ThemeSwitcher } from "@/components/ThemeSwitcher";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-100 dark:bg-zinc-900 p-24">
-      <ThemeSwitcher />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-100 dark:bg-zinc-900 p-24"></main>
   );
 }
