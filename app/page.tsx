37a9e02ce955ad079d3a9c4cfea4345cb06e6780
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-30 01:43:48 +0800

    :construction: Adds hero and about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import AboutSection from "@/sections/AboutSection";
+import HeroSection from "@/sections/HeroSection";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-100 dark:bg-zinc-900 p-24"></main>
+    <main className="min-h-screen bg-dotted-spacing-4 dark:bg-dotted-zinc-800 bg-dotted-slate-300 bg-slate-100 dark:bg-zinc-900 px-24">
+      <HeroSection />
+      <AboutSection />
+    </main>
   );
 }
