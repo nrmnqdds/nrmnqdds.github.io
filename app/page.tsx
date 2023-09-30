d8fc71b2fbcf925205c2774ed8c296c739ce1798
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-30 13:12:59 +0800

    :wheelchair: Add more sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import AboutSection from "@/sections/AboutSection";
 import HeroSection from "@/sections/HeroSection";
+import ProjectSection from "@/sections/ProjectSection";
+import SkillSection from "@/sections/SkillSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-dotted-spacing-4 dark:bg-dotted-zinc-800 bg-dotted-slate-300 bg-slate-100 dark:bg-zinc-900 px-24">
+    <main className="min-h-screen bg-dotted-spacing-4 dark:bg-dotted-zinc-800 bg-dotted-slate-300 bg-slate-100 dark:bg-zinc-900 px-5 md:px-24">
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
+      <SkillSection />
     </main>
   );
 }
