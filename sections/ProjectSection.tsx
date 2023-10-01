d2073c74c4b0a6f05fddca340d73d596bea739fa
qryskalyst20 <quddusnuriman@gmail.com>
2023-10-02 00:28:32 +0800

    :lipstick: Modify projects UI

--- sections/ProjectSection.tsx
+++ sections/ProjectSection.tsx
@@ -1,35 +1,40 @@
 "use client";
 
-import { GoRepo } from "react-icons/go";
 import Link from "next/link";
 import { RevealList } from "next-reveal";
 import { Image } from "@unpic/react";
+import { AiFillGithub } from "react-icons/ai";
 
 const projects = [
   {
     title: "Simplified i-Ma'luum",
     description: "A simplified version of student portal for my university!",
     link: "https://github.com/qryskalyst20/simplified-imaluum",
+    image: "/Screenshot 2023-09-19 221003.png",
   },
   {
     title: "X Profile",
     description: "Showcase your profile in a unique way!",
     link: "https://github.com/Youth-Icon/xprofile",
+    image: "/Screenshot 2023-10-01 233300.png",
   },
   {
     title: "FuelMY",
     description: "A fuel price tracker mobile app in Malaysia!",
     link: "https://github.com/qryskalyst20/fuelmy",
+    image: "/fuelmy.png",
   },
   {
     title: "DragonDex",
     description: "A Pokedex for Dragon City fans!",
-    link: "https://github.com/qryskalyst20/fuelmy",
+    link: "https://github.com/qryskalyst20/dragondex",
+    image: "/dd.png",
   },
   {
     title: "Rapidramatic",
     description: "Tracks RapidKL's breakdowns and delays!",
     link: "https://github.com/qryskalyst20/rapidramatic",
+    image: "/Screenshot 2023-10-01 231459.png",
   },
 ];
 
@@ -62,23 +67,42 @@ const ProjectSection = () => {
         origin="bottom"
         className="invisible w-full flex flex-row flex-wrap gap-2"
       >
-        {projects.map((project: any) => (
-          <Link
-            href={project.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            key={project.title}
-            className="bg-slate-300 dark:bg-zinc-950 border-2 border-solid border-purple-700 rounded-md drop-shadow-lg p-5 hover:bg-slate-400 dark:hover:bg-zinc-900"
+        {projects.map((project: any, index: number) => (
+          <div
+            key={index}
+            className={`py-10 flex ${
+              index % 2 === 0 ? `flex-row` : `flex-row-reverse`
+            } w-full`}
           >
-            <span className="flex items-center gap-2 text-purple-700 text-2xl">
-              <GoRepo />
-              <h1 className="font-bold">{project.title}</h1>
-            </span>
-
-            <p className="text-lg text-zinc-800 dark:text-slate-100">
-              {project.description}
-            </p>
-          </Link>
+            <div className="flex-1 flex flex-col items-center justify-center gap-3">
+              <h1 className="font-bold text-5xl text-zinc-800 dark:text-slate-100">
+                {project.title}
+              </h1>
+              <p className="font-bold text-2xl text-zinc-600 dark:text-zinc-400">
+                {project.description}
+              </p>
+              <Link
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex flex-row items-center gap-2 text-slate-100 bg-purple-700 py-3 px-2 rounded-xl hover:bg-purple-800"
+              >
+                <AiFillGithub size={20} />
+                <span>View on GitHub</span>
+              </Link>
+            </div>
+            <div className="flex-1 flex items-center justify-center">
+              <div className="rounded-lg overflow-hidden h-fit w-[500px]">
+                <Image
+                  src={project.image}
+                  alt=""
+                  width={0}
+                  height={0}
+                  className="w-full h-fill object-contain drop-shadow"
+                />
+              </div>
+            </div>
+          </div>
         ))}
       </RevealList>
     </section>
