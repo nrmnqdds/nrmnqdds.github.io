b38728144ceb247b365502d6b6254c5e75a05fcc
nrmnqdds <quddusnuriman@gmail.com>
2024-06-28 03:25:09 +0800

    :sparkles: remove lenis

--- src/providers/LenisProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactLenis } from "@studio-freight/react-lenis";
-
-export default function LenisProvider({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return <ReactLenis root>{children}</ReactLenis>;
-}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,17 +4,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Suspense } from "react";
-import LenisProvider from "../providers/LenisProvider";
 
 export const Route = createRootRoute({
 	component: () => (
 		<Suspense fallback={<Loading />}>
 			<QueryProvider>
-				<LenisProvider>
-					<Outlet />
-					<TanStackRouterDevtools />
-					<ReactQueryDevtools initialIsOpen={false} />
-				</LenisProvider>
+				<Outlet />
+				<TanStackRouterDevtools />
+				<ReactQueryDevtools initialIsOpen={false} />
 			</QueryProvider>
 		</Suspense>
 	),
