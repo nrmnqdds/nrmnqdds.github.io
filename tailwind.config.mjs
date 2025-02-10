5f7673d0a4c6dd094df46c2d08f491d119adec46
nrmnqdds <quddusnuriman@gmail.com>
2025-02-10 20:32:16 +0800

    :lipstick: better marquee

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "slide-left": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        "slide-left-infinite": "slide-left 15s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
